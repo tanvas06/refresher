3a7fa31e4041572ce5be6e05cc60c958909ef25d
Tanner Vasica <tmanvas06@hotmail.com>
2023-01-03 18:54:26 -0700

    begin functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from 'react';
+import './styles.css'
+import digitBtn from './digitBtn';
+import operationBtn from './operationBtn';
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, {type, payload}) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand || ""} ${payload.digit}`
+      }
+  }
+}
 
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <operationBtn operation="/" dispatch={dispatch} />
+      <digitBtn digit="1" dispatch={dispatch} />
+      <digitBtn digit="2" dispatch={dispatch} />
+      <digitBtn digit="3" dispatch={dispatch} />
+      <operationBtn operation="*" dispatch={dispatch} />
+      <digitBtn digit="4" dispatch={dispatch} />
+      <digitBtn digit="5" dispatch={dispatch} />
+      <digitBtn digit="6" dispatch={dispatch} />
+      <operationBtn operation="+" dispatch={dispatch} />
+      <digitBtn digit="7" dispatch={dispatch} />
+      <digitBtn digit="8" dispatch={dispatch} />
+      <digitBtn digit="9" dispatch={dispatch} />
+      <operationBtn operation="-" dispatch={dispatch} />
+      <digitBtn digit="." dispatch={dispatch} />
+      <digitBtn digit="0" dispatch={dispatch} />
+      <button className="span-two">=</button>
     </div>
   );
 }
